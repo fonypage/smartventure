8cd1c01c9cae84b949c2795f01ed3c062db1771c
Misha Ignatenko <misha.ignatenko.2017@bk.ru>
2025-07-15 18:04:32 +0300

    Initial commit

---
+++ src/main/java/com/smartventure/smartventure/SmartventureApplication.java
@@ -0,0 +1,13 @@
+package com.smartventure.smartventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartventure/smartventure/SmartventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartventure.smartventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
